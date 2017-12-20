523933efdb86490fdaac0d99bb6af363f24d66d1
BobbiJ <maximashka@yandex.ru>
2017-12-20 16:15:32 +0300

    Еще несколько задачек

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
@@ -1,9 +1,12 @@
 package com.javarush.task.task34.task3410.view;
 
 import com.javarush.task.task34.task3410.controller.EventListener;
+import com.javarush.task.task34.task3410.model.GameObject;
+import com.javarush.task.task34.task3410.model.GameObjects;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Set;
 
 public class Field extends JPanel{
     private View view;
@@ -19,6 +22,12 @@ public class Field extends JPanel{
 
 
    public void paint(Graphics g){
-
+        g.setColor(Color.BLACK);
+        g.fillRect(0,0,500,500);
+        GameObjects gameObjects = view.getGameObjects();
+        Set<GameObject> gameObjectSet =gameObjects.getAll();
+        for (GameObject gameObject:gameObjectSet){
+            gameObject.draw(g);
+        }
    }
 }
